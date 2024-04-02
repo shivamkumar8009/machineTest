b8ca284152f0cd1b4b1cec89f222ddacd29735df
Shivam <exceptionunique@gmail.com>
2024-04-02 12:33:06 +0530

    routing done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,41 @@
+import { Link } from "react-router-dom";
 import styles from "./Header.module.css";
 
 function Header() {
   return (
     <>
       <section className={styles.header}>
-        <div className={styles.left}>Movie DB</div>
+        <div className={styles.left}>
+          <Link className={styles.l1} to="/">
+            Movie DB
+          </Link>
+        </div>
         <div className={styles.right}>
-          <div className={styles.r1}>Popular</div>
-          <div className={styles.r2}>Top Rated</div>
-          <div className={styles.r3}>Upcoming</div>
-          <div className={styles.r4}>Movie Name</div>
-          <div className={styles.r5}>Search</div>
+          <div>
+            <Link className={styles.r1} to="/">
+              Popular
+            </Link>
+          </div>
+          <div>
+            <Link className={styles.r2} to="/topRated">
+              Top Rated
+            </Link>
+          </div>
+          <div>
+            <Link className={styles.r3} to="/upcoming">
+              Upcoming
+            </Link>
+          </div>
+          <div>
+            <Link className={styles.r4} to="/">
+              Movie Name
+            </Link>
+          </div>
+          <div>
+            <Link className={styles.r5} to="/">
+              Search
+            </Link>
+          </div>
         </div>
       </section>
     </>
